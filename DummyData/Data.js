89988e6c79249dee573251c8bf946b26041806ef
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-06-28 04:35:23 -0400

    created dummy data for markers

---
+++ DummyData/Data.js
@@ -0,0 +1,69 @@
+export const Locations = [
+  {
+    title: '',
+    latitude: 40.72211113,
+    longitude: -73.9524,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.72221115,
+    longitude: -73.9524,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.72231117,
+    longitude: -73.9524,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7098,
+    longitude: -73.9441,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7098,
+    longitude: -73.9441,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7098,
+    longitude: -73.9443,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0006,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0006,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0009,
+    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    weight: 1,
+  },
+];
+
+for (let i = 0; i < Locations.length; i++) {
+  Locations[i].title = `Player ${i + 1}`;
+}
