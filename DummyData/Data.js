058fef7bb638fec4d0a70c654688a5d9c0baff36
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-06-28 05:18:50 -0400

    added more dummy data

--- DummyData/Data.js
+++ DummyData/Data.js
@@ -3,63 +3,152 @@ export const Locations = [
     title: '',
     latitude: 40.72211113,
     longitude: -73.9524,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
   {
     title: '',
     latitude: 40.72221115,
     longitude: -73.9524,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
   {
     title: '',
     latitude: 40.72231117,
     longitude: -73.9524,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
   {
     title: '',
     latitude: 40.7098,
     longitude: -73.9441,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
   {
     title: '',
     latitude: 40.7098,
     longitude: -73.9441,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7098,
+    longitude: -73.9441,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7098,
+    longitude: -73.9441,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7098,
+    longitude: -73.9441,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7098,
+    longitude: -73.9441,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
   {
     title: '',
     latitude: 40.7098,
     longitude: -73.9443,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
   {
     title: '',
     latitude: 40.7314,
     longitude: -74.0006,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
   {
     title: '',
     latitude: 40.7314,
     longitude: -74.0006,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
   {
     title: '',
     latitude: 40.7314,
     longitude: -74.0009,
-    markerImage: 'https://i.ibb.co/2PwKJWc/UA-Studios-2.png',
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0006,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0006,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0009,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0006,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0006,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
+    weight: 1,
+  },
+  {
+    title: '',
+    latitude: 40.7314,
+    longitude: -74.0009,
+    markerImage:
+      'https://i.pinimg.com/originals/10/d7/36/10d736dcd7b4562b94bd42cb3a172eff.png',
     weight: 1,
   },
 ];
