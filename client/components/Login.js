90d837b28cb2b1a68f2fd35f3a59c32dedb51ef5
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-06-29 05:28:40 -0400

    implemented basic css for log in screen

--- client/components/Login.js
+++ client/components/Login.js
@@ -5,13 +5,38 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import {HomeFeed} from './loggedInComponents/HomeFeed';
+import {TextInput} from 'react-native-paper';
+
+import {GreetingStyles} from '../../public/stylesheets/allScreens';
 
 const LogInStack = createStackNavigator();
 
 const LogIn = ({navigation}) => {
   return (
-    <View style={Styles.center}>
-      <Text>Sign up</Text>
+    <View style={GreetingStyles.container}>
+      <View style={GreetingStyles.header}>
+        <Text style={GreetingStyles.text_header}>Welcome!</Text>
+      </View>
+      <View style={GreetingStyles.footer}>
+        <Text style={GreetingStyles.text_footer}>Email</Text>
+        <View style={GreetingStyles.action}>
+          <TextInput
+            placeholder="Email"
+            style={GreetingStyles.textInput}
+            autoCapitalize="none"
+          />
+        </View>
+        <Text style={[GreetingStyles.text_footer, {marginTop: 35}]}>
+          Password
+        </Text>
+        <View style={GreetingStyles.action}>
+          <TextInput
+            placeholder="Password"
+            style={GreetingStyles.textInput}
+            autoCapitalize="none"
+          />
+        </View>
+      </View>
       <Button title="Sign up" onPress={() => navigation.navigate('Home')} />
     </View>
   );
@@ -35,7 +60,7 @@ export class LogInScreen extends Component {
           },
         }}>
         <LogInStack.Screen
-          name="Sign up"
+          name="Log in"
           component={LogIn}
           options={{
             headerLeft: () => (
