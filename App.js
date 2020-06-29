071f6278eb8b6ff7905e7b853d0f95a930e96920
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-06-29 04:13:47 -0400

    plugged new drawer nav

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import {SignUpScreen} from './client/components/SignUp';
 
 import {BottomTabs} from './client/components/loggedInComponents/BottomTabs';
+import {DrawerContent} from './client/components/loggedInComponents/DrawerContent';
 
 export default class App extends Component {
   render() {
@@ -29,7 +30,7 @@ export default class App extends Component {
 
     return (
       <NavigationContainer>
-        <Drawer.Navigator>
+        <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
           {/* Use check here to see if user has auth token, if so show Home, else show Sign up  */}
           <Drawer.Screen name="Home" component={BottomTabs} />
           <Drawer.Screen name="Sign up" component={SignUpScreen} />
