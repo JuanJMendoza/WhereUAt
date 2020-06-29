2c641818c5ec8a178f7f694a7c8c9af8ce61649f
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-06-28 21:41:01 -0400

    implemented drawer & stack navigation

--- App.js
+++ App.js
@@ -19,18 +19,22 @@ import MapView, {PROVIDER_GOOGLE, Marker, Heatmap} from 'react-native-maps';
 import {CustomMarker} from './source/CustomMarker';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import {HeatMap} from './client/components/HeatMap';
+import {SignUpScreen} from './client/components/SignUp';
 
 export default class App extends Component {
   render() {
-    const Stack = createStackNavigator();
+    const Drawer = createDrawerNavigator();
 
     return (
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="HeatMap" component={HeatMap} />
-        </Stack.Navigator>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Sign up" component={SignUpScreen} />
+          <Drawer.Screen name="HeatMap" component={HeatMap} />
+        </Drawer.Navigator>
       </NavigationContainer>
     );
   }
